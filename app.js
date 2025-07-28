a7269eca55b45dd7ac5d5d78a0e83c82f256ea08
KevinPieplu <pieplu.kevin@gmail.com>
2025-07-28 16:02:00 +0200

    feat: branchement de la base de donnée a l'app

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const app = express();
+const mongoose = require('mongoose');
 
 const authRoutes = require('./routes/auth');
 const booksRoutes = require('./routes/books');
+
+const MONGO_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGO_URI,
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', authRoutes);
