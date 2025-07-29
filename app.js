7a3c85637b9ab79f1dd1f85f63446773f73440f8
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-07-29 08:50:49 +0200

    Feat: gestion des erreurs CORS

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express")
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const authRoutes = require('./routes/auth');
 const booksRoutes = require('./routes/books');
 
-const MONGO_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGO_URI,
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL,
     { useNewUrlParser: true,
     useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use(cors())
+app.use(express.json())
 
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', authRoutes);
