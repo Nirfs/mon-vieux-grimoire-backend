b8b92ee19e37c17877fbd8fbe6dfc10322e915be
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-07-29 09:05:13 +0200

    feat: GET getBook et getBestRating

--- controllers/books.js
+++ controllers/books.js
@@ -2,17 +2,31 @@ const Book = require('../models/books');
 const fs = require('fs')
 
 exports.getAllBooks = (req, res) => {
+    //Recuperer tout les livre de la base de donnée
     Book.find()
         .then(book => res.status(200).json(book))
         .catch(error => res.status(400).json({error}))
 };
 
-exports.getBook = (req, res) => {
-
+exports.getBook = (req, res) => {   
+    //Recuperer un livre de la base de donnée -> _id définit par mongodb pour chaque item =  a l'id de l'url (req.params.id)
+    Book.findOne({_id: req.params.id})
+        .then(book => {
+            if (!book){
+                return res.status(404).json({ message: 'Livre non trouvé !' })
+            }
+            res.status(200).json(book)
+        })
+        .catch(error => res.status(400).json({error}))
 };
 
 exports.getBestRating = (req, res) => {
-
+    //Recuperer tout les livre de la base de donnée, tri dans l'ordre décroissant, prend les 3 premier
+    Book.find()
+        .sort({averageRating: -1})
+        .limit(3)
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({error}))
 };
 
 exports.createBook = (req, res) => {
